73ba8ab550903cb80c8eb00e71da58ff59e5d4d4
Vishnu Muraleedharan <vishnu.muraleedharan@gmail.com>
2023-12-23 12:57:25 +0000

    Added assignement skelton

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,135 @@
-#include "imgui.h"
+#include <fstream>
+#include <iostream>
+#include <memory>
+
 #include "imgui-SFML.h"
+#include "imgui.h"
+#include <SFML/Graphics.hpp>
 
-#include <SFML/Graphics/CircleShape.hpp>
-#include <SFML/Graphics/RenderWindow.hpp>
-#include <SFML/System/Clock.hpp>
-#include <SFML/Window/Event.hpp>
+int
+main(int argc, char* argv[])
+{
+  // create a wundow of size w*h pixels
+  // top-left of the window is (0,0) and bottom-right is (w,h)
+  // you will have to read these from the config file
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(640, 480), "ImGui + SFML = <3");
-    window.setFramerateLimit(60);
-    static_cast<void>(ImGui::SFML::Init(window));
+  const int wWidth = 1280;
+  const int wHeight = 720;
 
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+  sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Works!");
+  window.setFramerateLimit(60);
 
-    sf::Clock deltaClock;
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            ImGui::SFML::ProcessEvent(window, event);
+  // initialize IMGUI and create a clock used for its timing
+  ImGui::SFML::Init(window);
+  sf::Clock deltaClock;
 
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
+  // scale the imgui ui by a give factor, does not affect text size
+  ImGui::GetStyle().ScaleAllSizes(1.0f);
+
+  // the imgui color {r, g, b} wheel requires floats from 0-1
+  float c[3] = { 0.0f, 1.0f, 1.0f };
+
+  // shape
+  float circleRadius = 50;
+  int circleSegments = 32; // number of segments to draw the circle
+  float circleVelocityX = 1.0f;
+  float circleVelocityY = 0.5f;
+  bool drawCircle = true;
+  bool drawText = true;
+
+  // create the sfml circle shape based on our paramteres
+  sf::CircleShape circle(circleRadius, circleSegments);
+
+  sf::Font myFont;
+
+  // attempt to load the font from a file
+  if (!myFont.loadFromFile("fonts/nirmala.ttf")) {
+    // if we can't load the font exit
+    std::cerr << "Could not load font\n";
+    exit(-1);
+  }
+
+  sf::Text text("Sample Text", myFont, 24);
+
+  text.setPosition(0, wHeight - (float)text.getCharacterSize());
+
+  // set up a charcter array to set the text
+  char displayString[255] = "Sample Text";
+
+  // main loop - continues for each frame, while window is open
+  while (window.isOpen()) {
+
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      ImGui::SFML::ProcessEvent(window, event);
+
+      // This event is treggered when window is closed
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
+
+      // this event is triggered when any key is pressed
+      if (event.type == sf::Event::KeyPressed) {
+
+        // print the key that was pressed to the console
+        std::cout << " Key pressed with code =" << event.key.code << "\n";
+
+        if (event.key.code == sf::Keyboard::X) {
+
+          // reverse the x direction of the circle on the screen
+          circleVelocityX *= -1.0f;
         }
-
-        ImGui::SFML::Update(window, deltaClock.restart());
-
-        ImGui::ShowDemoWindow();
-
-        ImGui::Begin("Hello, world!");
-        ImGui::Button("Look at this pretty button");
-        ImGui::End();
-
-        window.clear();
-        window.draw(shape);
-        ImGui::SFML::Render(window);
-        window.display();
+      }
     }
 
-    ImGui::SFML::Shutdown();
+    // update imgui of this frame with the time that the last frame took
+    ImGui::SFML::Update(window, deltaClock.restart());
+
+    ImGui::Begin("Window title");
+    ImGui::Text("Window text!");
+    ImGui::Checkbox("Draw Circle", &drawCircle);
+    ImGui::SameLine();
+    ImGui::Checkbox("Draw Text", &drawText);
+    ImGui::SameLine();
+    ImGui::SliderFloat("Radius", &circleRadius, 0.0f, 300.0f);
+    ImGui::SliderInt("Sides", &circleSegments, 3, 64);
+    ImGui::ColorEdit3("Color Circle", c);
+    ImGui::InputText("Text", displayString, 255);
+
+    if (ImGui::Button("Set Text")) {
+      text.setString(displayString);
+    }
+    ImGui::SameLine();
+
+    if (ImGui::Button("Reset Circle")) {
+      circle.setPosition(0, 0);
+    }
+    ImGui::End();
+
+    // set the circle properies because they may have been updated with the ui
+    circle.setFillColor(sf::Color(c[0] * 255, c[1] * 255, c[2] * 255));
+    circle.setPointCount(circleSegments);
+    circle.setRadius(circleRadius);
+
+    // basic animation - move in each frame
+    circle.setPosition(circle.getPosition().x + circleVelocityX,
+                       circle.getPosition().y + circleVelocityY);
+
+    // basic rendering function calls
+    window.clear();
+    if (drawCircle) {
+
+      window.draw(circle);
+    }
+
+    if (drawText) {
+
+      window.draw(text);
+    }
+
+    ImGui::SFML::Render(window);
+    window.display();
+  }
+
+  return 0;
 }
